4d68d0525fbfcebf05835319df4fb587655918fd
ryi01 <55128336+ryi01@users.noreply.github.com>
2019-11-07 15:26:02 +0900

    10_op2

--- main.c
+++ main.c
@@ -5,21 +5,12 @@
 
 int main(int argc, char *argv[]) {
 	
-	int i = 10;
-	char c ='a';
+	int i = 4;
 	
-	int *iptr;
-	iptr=&i;
+	int *pi=&i;
+	char *pc=&i;
 	
-	char *cptr;
-	cptr = &c;
-	
-	int *iptr2 = iptr;
-	
-	
-	printf("i : %p\n%p (size : %i)\n", iptr, &i, sizeof(iptr));
-	printf("c : %p\n%p (size : %i)\n", cptr, &c, sizeof(cptr));
-	printf("iptr2 : %p, %i\n", iptr2, *iptr);
+	printf("%i, %i, %i\n", i, *pi, *pc);
 	
 	return 0;
 }
